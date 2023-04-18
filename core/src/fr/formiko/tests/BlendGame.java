1df77dd56526823bf6db2f15b79368c863d6bf4b
HydrolienF <hydrolien.f@gmail.com>
2023-04-18 10:31:15 +0200

    working versio with Sprite

--- core/src/fr/formiko/tests/BlendGame.java
+++ core/src/fr/formiko/tests/BlendGame.java
@@ -4,48 +4,85 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class BlendGame extends ApplicationAdapter {
-	SpriteBatch batch;
+	SpriteBatch spriteBatch;
 	Texture donut;
 	Texture visible;
+	private Sprite mask, maskedSprite;
 
 	@Override
 	public void create() {
-		batch = new SpriteBatch();
+		spriteBatch = new SpriteBatch();
 		donut = new Texture("donut.png");
 		visible = new Texture("visible.png");
+		// Gdx.gl.glEnable(GL20.GL_BLEND);
+
+		/* Load the mask containing the alpha information. */
+		mask = new Sprite(new Texture("visible.png"));
+
+		/* Load the sprite which will be masked. */
+		maskedSprite = new Sprite(new Texture("donut.png"));
 	}
 
 	@Override
 	public void render() {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
+		ScreenUtils.clear(0, 1, 0, 1);
+		spriteBatch.begin();
 
 
 		// for the mask
 		Gdx.gl.glColorMask(false, false, false, true);
-		batch.setBlendFunction(GL20.GL_ONE, GL20.GL_ZERO);
-		batch.draw(visible, 0, 0);
+		// spriteBatch.setBlendFunction(GL20.GL_ONE, GL20.GL_ZERO);
+		// spriteBatch.draw(visible, 0, 0);
+		// spriteBatch.flush();
+		/* Change the blending function for our alpha map. */
+		spriteBatch.setBlendFunction(GL20.GL_ONE, GL20.GL_ZERO);
 
-		batch.flush();
+		/* Draw alpha masks. */
+		mask.draw(spriteBatch);
+
+		/* This blending function makes it so we subtract instead of adding to the alpha map. */
+		spriteBatch.setBlendFunction(GL20.GL_ZERO, GL20.GL_SRC_ALPHA);
+
+		/* Remove the masked sprite's inverse alpha from the map. */
+		maskedSprite.draw(spriteBatch);
+
+		/* Flush the batch to the GPU. */
+		spriteBatch.flush();
 
 
 		// masked
 		Gdx.gl.glColorMask(true, true, true, true);
-		batch.setBlendFunction(GL20.GL_DST_ALPHA, GL20.GL_ONE_MINUS_DST_ALPHA);
-		batch.draw(donut, 0, 0);
-		batch.flush();
+		//
+		// spriteBatch.setBlendFunction(GL20.GL_DST_ALPHA, GL20.GL_ONE_MINUS_DST_ALPHA);
+		// spriteBatch.setBlendFunctionSeparate(GL20.GL_DST_ALPHA, GL20.GL_ONE_MINUS_DST_ALPHA, GL20.GL_DST_ALPHA,
+		// GL20.GL_ONE_MINUS_DST_ALPHA);
+
+		// spriteBatch.setBlendFunction(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+
+		// spriteBatch.setBlendFunctionSeparate(GL20.GL_DST_ALPHA, GL20.GL_ONE_MINUS_DST_ALPHA, GL20.GL_ZERO, GL20.GL_ONE_MINUS_DST_ALPHA);
+		// spriteBatch.draw(donut, 0, 0);
+		// spriteBatch.flush();
+		/* Change the blending function so the rendered pixels alpha blend with our alpha map. */
+		spriteBatch.setBlendFunction(GL20.GL_DST_ALPHA, GL20.GL_ONE_MINUS_DST_ALPHA);
+
+		/* Draw our sprite to be masked. */
+		maskedSprite.draw(spriteBatch);
+
+		/* Remember to flush before changing GL states again. */
+		spriteBatch.flush();
 
 
-		batch.end();
+		spriteBatch.end();
 	}
 
 	@Override
 	public void dispose() {
-		batch.dispose();
+		spriteBatch.dispose();
 		donut.dispose();
 	}
 }
